0f8abf77bd1010f8b8d7bb9df942637d84bed25b
Gopiverse <gopibaae05@gmail.com>
2026-06-27 23:03:38 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c) || Character.isLetter(c)) {
+                str += Character.toLowerCase(c);
+            }
+        }
+        int start = 0;
+        int end = str.length()-1;
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
