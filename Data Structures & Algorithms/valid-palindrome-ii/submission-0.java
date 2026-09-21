8064ca5ba42bdff4159f12c8d8ffe85be39a2c46
Gopiverse <gopibaae05@gmail.com>
2026-09-22 00:15:13 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        if(isPalindrome(s)) return true;
+        for(int i=0; i<s.length(); i++) {
+            String newS = s.substring(0,i) + s.substring(i+1);
+            if(isPalindrome(newS)) return true;
+        }
+        return false;
+    }
+}
