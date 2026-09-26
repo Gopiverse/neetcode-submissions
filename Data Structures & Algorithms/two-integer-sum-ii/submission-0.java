a6eae5477f1f1f82d2b35c8aefff21e2d205c6f2
Gopiverse <gopibaae05@gmail.com>
2026-09-27 00:41:30 +0530

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -5,16 +5,12 @@ class Solution {
 
         while(low < high) {
             int sum = numbers[low] + numbers[high];
-            if(sum == target) {
-                return new int[]{low+1, high+1};
-            }
-            else if(sum < target) {
-                low++;
-            }
-            else if(sum > target) {
-                high--;
-            }
+            if(sum == target) return new int[]{low+1, high+1};
+            else if(sum < target) low++;
+            else if(sum > target) high--;
         }
+
         return new int[]{};
+    
     }
 }
